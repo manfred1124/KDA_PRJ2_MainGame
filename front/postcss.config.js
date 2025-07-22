0a1d49d1ac190796383434a3cea70d10a4fbe5f3
Jeongu <zxczxc97@naver.com>
2025-07-22 21:36:51 +0900

    Dockerfile, postcss.config.js 수정

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,6 +1,14 @@
-export default {
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+/** @type {import('postcss').ProcessorOptions} */
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} 
+};
